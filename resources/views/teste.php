7a5fc76bc0ab884371087c5eae9a7ff5a2f1e189
Antonio <antonioimportant@gmail.com>
2024-03-02 10:43:47 -0300

    created head and footer with dynamic title for routes

--- resources/views/teste.php
+++ resources/views/teste.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
-  <h1>Teste</h1>
-</body>
-
-</html>
+<?php include_once "components/head.php" ?>
+<h1 class="bg-black">Teste</h1>
+<?php include_once "components/footer.php" ?>
